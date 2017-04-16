ca6fcbc75e3321cca4012ce8d378aabe62221e97
warglaivee <warglaivegm@gmail.com>
2017-04-16 23:48:39 +0300

    PC written 01. Serialize String

---
+++ 01. Serialize String/01. Serialize String.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01.Serialize_String
+{
+    public class Program
+    {
+       public static void Main()
+        {
+            var input = Console.ReadLine();
+            var character = new HashSet<char>();
+            var toChar = input.ToCharArray();
+            foreach (var item in input)
+            {
+                character.Add(item);
+            }
+            var result = string.Empty;
+            foreach (var itemChar in character)
+            {
+                result += itemChar + ":";
+                var index = input.IndexOf(itemChar);
+                while (index >= 0) 
+                {
+                    result += index + "/";
+                    index = input.IndexOf(itemChar, index + 1);
+                }
+                Console.WriteLine(result.Trim('/'));
+                result = string.Empty;
+            }
+        }
+    }
+}
