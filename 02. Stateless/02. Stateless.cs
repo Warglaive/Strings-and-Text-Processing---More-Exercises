5dcc8674140b890f97b3e492ee8426a2ef982763
warglaivee <warglaivegm@gmail.com>
2017-04-18 21:40:49 +0300

    02. Stateless written PC

---
+++ 02. Stateless/02. Stateless.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Stateless
+{
+   public class Program
+    {
+       public static void Main()
+        {
+            var input = Console.ReadLine();
+            while (input!= "collapse")
+            {
+                var states = input;
+                var fiction = Console.ReadLine();
+
+                while (fiction.Length > 0)
+                {
+                    states = states.Replace(fiction, "");
+                    fiction = fiction.Remove(0, 1);
+                    if (fiction.Length>0)
+                    {
+                        fiction = fiction.Remove(fiction.Length - 1, 1);
+                    }
+                    
+                }
+                if (states == string.Empty)
+                {
+                    Console.WriteLine("(void)");
+                }
+                else
+                {
+                    Console.WriteLine(states.Trim());
+                }
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
